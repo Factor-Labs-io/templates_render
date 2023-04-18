9c7e2965ffaa0a1752048e96615743ca0c5be0a3
Darius Ramnath <39842396+Dariusramnath@users.noreply.github.com>
2023-04-18 16:32:41 -0400

    setup
    -new App.js
    -addition of useful components to build faster

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello from Render!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <h1>Hello, World</h1>
+  </div>
+);
 
 export default App;
